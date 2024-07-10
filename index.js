5bea96935a7b42aea369d5934a9327d15b1de516
MdShamshad786 <136726076+MdShamshad786@users.noreply.github.com>
2024-07-10 09:50:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function about(){
+    document.getElementById("about-me").scrollIntoView({behavior:"smooth"})
+}
+function service(){
+    document.getElementById("service()").scrollIntoView({behavior:"smooth"})
+}
+function contact(){
+    document.getElementById(" contact()").scrollIntoView({behavior:"smooth"})
+}
